50a15e0569cf2e519016a3016c38aca690d6e9c1
gamitej <gamitejpratapsingh998@gmail.com>
2023-08-18 12:45:33 +0530

    responsive

--- src/pages/Chat/index.jsx
+++ src/pages/Chat/index.jsx
@@ -6,13 +6,13 @@ import ChatBodyEmpty from "./ChatBodyEmpty";
 const Chat = ({ isChatBody = true }) => {
   return (
     <div className="flex justify-center items-center w-full h-[100vh]">
-      <div className="h-[calc(100%-2.5rem)] w-[calc(100%-15rem)] flex shadow-lg">
+      <div className="h-[calc(100%-2.5rem)] w-[95%] sm:w-[calc(100%-2rem)] md:w-[calc(100%-5rem)] lg:w-[calc(100%-7rem)] xl:w-[calc(100%-12rem)] flex shadow-lg">
         {/* chat left side */}
         <ChatLeftSide className="w-[30%] h-full hidden sm:block" />
         {/* chat body */}
         {isChatBody && (
           <ChatBody
-            className="w-[70%] h-full"
+            className="w-[100%] sm:w-[70%] h-full"
             style={{
               backgroundColor: colorShades.grey.main,
               borderLeft: `0.5px solid ${colorShades.grey["800"]}`,
@@ -22,7 +22,7 @@ const Chat = ({ isChatBody = true }) => {
         {/* chat body when no chat selected */}
         {!isChatBody && (
           <ChatBodyEmpty
-            className="w-[70%] h-full flex flex-col justify-center items-center gap-y-[3rem]"
+            className="w-[100%] sm:w-[70%] h-full flex flex-col justify-center items-center gap-y-[3rem]"
             style={{
               backgroundColor: colorShades.grey.main,
               borderLeft: `0.01rem solid ${colorShades.grey["800"]}`,
